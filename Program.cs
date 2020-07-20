939266452d565e1630c271c65eb6d94e0179b4d6
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-07-20 15:31:52 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Playing_Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FiveCardDraw game = new FiveCardDraw();
+            game.Game();
+        }
+    }
+}
